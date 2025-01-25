4e386eb3c7f2da6b5ad207f0b05a1fab74c8fbf1
Komal Killedar <sunny.r@iworktech.com>
2025-01-25 15:37:07 +0530

    add .env file

--- src/App.js
+++ src/App.js
@@ -6,30 +6,31 @@ import Board from "./components/Board"
 import Home from "./components/Home"
 
 export default function App() {
+
   const [user, setUser] = useState(null); // Store the logged-in user's data
 
+  console.log(1,process.env.REACT_APP_API_KEY)
+
   const handleAuthChange = (authUser) => {
-    setUser(authUser); // Update the state when a user logs in or registers
+    setUser(authUser); 
   };
 
   return (
     <Router>
       <div className="App">
         <Routes>
-          {/* Default route: Redirect to login if user isn't authenticated */}
           <Route
             path="/"
             element={user ? <Board user={user} /> : <Navigate to="/login" />}
           />
           
 
-          {/* Login Page */}
           <Route
             path="/login"
             element={<Login onLoginSuccess={handleAuthChange} />}
           />
 
-          {/* Register Page */}
+        
           <Route
             path="/register"
             element={<Register onRegisterSuccess={() => (window.location.href = "/login")} />}
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,18 +2,23 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDpcFoOnCkojDuT_BwsBaSU2DMnbgU5khc",
-  authDomain: "kanban-board-application-a3141.firebaseapp.com",
-  projectId: "kanban-board-application-a3141",
-  storageBucket: "kanban-board-application-a3141.firebasestorage.app",
-  messagingSenderId: "893782058608",
-  appId: "1:893782058608:web:d8dd2ad0d77438ffe3b6b5"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
+
+
+console.log("apikey", firebaseConfig.apiKey);
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
